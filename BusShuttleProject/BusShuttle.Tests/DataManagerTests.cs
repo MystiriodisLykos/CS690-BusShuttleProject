ade9c8fcab1bd5b6c8521cb37d2591298a9b0bad
Brendan DeLeeuw <13935754+MystiriodisLykos@users.noreply.github.com>
2026-02-04 12:26:41 -0700

    adding driver management to DataManager.cs
    Tests for driver management in DataManager

--- BusShuttleProject/BusShuttle.Tests/DataManagerTests.cs
+++ BusShuttleProject/BusShuttle.Tests/DataManagerTests.cs
@@ -8,6 +8,7 @@ public class DataManagerTests
 
     public DataManagerTests() {
         File.WriteAllText("stops.txt","One"+Environment.NewLine+"Two"+Environment.NewLine+"Three"+Environment.NewLine+"Four"+Environment.NewLine+"Five");
+        File.WriteAllText("drivers.txt","Driver1"+Environment.NewLine+"Driver2"+Environment.NewLine+"Driver3");
         dataManager = new DataManager();
     }
 
@@ -19,4 +20,45 @@ public class DataManagerTests
         Assert.Equal(6,dataManager.Stops.Count);
     }
 
+    [Fact]
+    public void Test_DataManager_loads_drivers_from_file()
+    {
+        Assert.Equal(3,dataManager.Drivers.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_can_add_driver()
+    {
+        dataManager.AddDriver(new Driver("new driver"));
+        Assert.Equal(4,dataManager.Drivers.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_can_remove_driver()
+    {
+        dataManager.RemoveDriver(dataManager.Drivers[0]);
+        Assert.Equal(2,dataManager.Drivers.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_add_driver_persists()
+    {
+        dataManager.AddDriver(new Driver("new driver"));
+
+        dataManager = null;
+        dataManager = new DataManager();
+
+        Assert.Equal(4,dataManager.Drivers.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_remove_driver_persists()
+    {
+        dataManager.RemoveDriver(dataManager.Drivers[0]);
+
+        dataManager = null;
+        dataManager = new DataManager();
+
+        Assert.Equal(2,dataManager.Drivers.Count);
+    }
 }
